a184ede0e3a08ae25f5d0c8a59b213216c52af62
Kevin Chau <kcheez@Kevins-MacBook-Pro.local>
2018-09-07 17:49:35 -0700

    created script to insert dummy data into db

---
+++ seed_data/databaseLoader.js
@@ -0,0 +1,24 @@
+const MongoClient = require('mongodb');
+const dummyData = require('./zagat-data.json');
+
+const url = 'mongodb://localhost:27017';
+
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      console.log('error to the db');
+    } else {
+      console.log('success to the db');
+      const db = client.db('zagat');
+      db.collection('restaurants').insertMany(dummyData, (err, res) => {
+        if (err) {
+          console.log('error', err);
+        } else {
+          console.log('success!');
+          client.close();
+        }
+      });
+    }
+  });
