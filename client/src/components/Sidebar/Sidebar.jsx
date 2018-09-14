60d1eef77d78222ba114919a86ce219f783a6849
Kevin Chau <kcheez@Kevins-MacBook-Pro.local>
2018-09-13 17:31:18 -0700

    rendered all data on sidebar without css

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import './Sidebar.css';
+import Hours from './Hours.jsx';
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    
-  }
-
-  render() {
-    return (
-      <div className="Sidebar">
-       Sidebar dawg
+const Sidebar = (props) => {
+  return (
+    <div className="Sidebar">
+      <div>
+        <Hours times={props.info.times}/>
       </div>
-    );
-  }
-}
-
+      <div>
+        {props.info.location ? props.info.location.address : 'Loading'}, San Francisco, CA 94112, USA 
+      </div>
+      <div>
+        {props.info.phone ? `(${props.info.phone.slice(0, 3)}) ${props.info.phone.slice(4)}` : 'Loading'}
+      </div>
+      <div>
+        {props.info.website}
+      </div>
+      <div>Get Directions</div>
+    </div>
+  );
+};
 
 export default Sidebar;
