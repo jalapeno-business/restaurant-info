25d0acd89ed893735728b0930e93fdb69a7c84c7
Kevin Chau <kcheez@Kevins-MacBook-Pro.local>
2018-09-13 17:30:50 -0700

    wrote functions to pull data from db without running into async issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,44 @@ import Zagat from './components/Dummy/Zagat.jsx';
 import Carousel from './components/Dummy/Carousel.jsx';
 import Publications from './components/Dummy/Publications.jsx';
 import Reviews from './components/Dummy/Reviews.jsx';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      restaurant: null
+    };
+  }
+
+  getBusinessInfo(id) {
+    axios.get(`/restaurant/${id}/info`)
+      .then((response) => {
+        this.setState({
+          restaurant: response.data
+        });
+      });
+  }
+
+  generateRandomID() {
+    return Math.floor(Math.random() * 100);
+  }
+
+  componentDidMount() {
+    this.getBusinessInfo(this.generateRandomID());
   }
 
   render() {
+    if (this.state.restaurant === null) {
+      return <div />;
+    }
+    // const { businessInfo } = this.state.restaurant;
     return (
       <div className="app">
         <div className="info">
-          <Sidebar />
+          <Sidebar info={this.state.restaurant ? this.state.restaurant.businessInfo : 'loading'} />
           
         </div>
         <div className="suggestions">
